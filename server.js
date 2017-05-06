276aca3dee5a61cbf5fd331f45ccb39f85762914
Jason Chandler <jasonmichaelchandler@gmail.com>
2017-05-06 00:27:46 +0000

    final

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var app = express()
 
 app.get("/", function (req, res) {
   var obj = {}
-  obj.ip = req.headers["x-forwarded-for"]
-  obj.lang = req.headers['accept-language'].slice(0,5)
-  obj.userAgent = req.headers['user-agent']
-  res.send({"ipaddress":obj.ip, "language":obj.lang, "software":obj.userAgent})
+  obj.ipaddress = req.headers["x-forwarded-for"]
+  obj.language = req.headers['accept-language'].slice(0,5)
+  obj.software = req.headers['user-agent']
+  res.send(obj)
 })
 
 app.listen(process.env.PORT, process.env.IP)
